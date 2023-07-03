8d59a463b7a7ba67fbc8be0831d1afcfe8bd2d6c
Matthew Sah <mattsah2001@gmail.com>
2023-07-04 07:24:33 +0800

    completed navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import './index.css'
 
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
