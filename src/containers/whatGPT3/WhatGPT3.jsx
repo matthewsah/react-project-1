b3633cfae6615c826183ebc295bf540e532ef891
Matthew Sah <mattsah2001@gmail.com>
2023-07-04 17:14:23 +0800

    finished what is gpt3 section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,16 +6,16 @@ const WhatGPT3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
       </div>
       <div className="gpt3__whatgpt3-heading">
         <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
         <p>Explore The Library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard on ought."/>
+        <Feature title="Knowledgebase" text="These are some words. There is not much to be said here though."/>
+        <Feature title="Education" text="It would be good to get educated on the topic of AI. AI is quickly becoming a widely-used tool in across all industries."/>
       </div>
     </div>
   )
