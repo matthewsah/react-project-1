250b3b461600bce661bc1ab83dda21afcfef3b37
Matthew Sah <mattsah2001@gmail.com>
2023-07-06 15:43:12 +0800

    completed website

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import './footer.css'
+import gpt3Logo from '../../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">Do you want to step into the future before others</h1>
+      </div>
+
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="logo"/>
+          <p>All rights reserved</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+
+        <div className="gpt3__footer-company_div">
+          <h4>Company</h4>
+          <p>Terms and Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+
+        <div className="gpt3__footer-company_div">
+          <h4>Get in touch</h4>
+          <p>123 40th St, Newport Beach, CA 92663</p>
+          <p>(123)-456-7890</p>
+          <p>mattsah2001@gmail.com</p>
+        </div>
+      </div>
+
+      <div className="gpt3__footer-copyright">
+        <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
     </div>
   )
 }
